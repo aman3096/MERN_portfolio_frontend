4483da5ff38d628b8f71ad8445a5551832daa39a
Aman Tandon <aman.tandon3096@gmail.com>
2022-05-10 17:42:27 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import {Link } from "react-router-dom";
+import Logo from "../assets/logo.png"
 const Navbar = () => {
   return (
     <>
      <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    Navbar
+    <img src={Logo}/>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
